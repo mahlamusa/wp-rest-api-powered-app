920a380d247d1c02b4bc39ac0b7a6b8e38b1db18
mahlamusa <mahlamusa@gmail.com>
2018-11-08 20:16:25 +0200

    Use demo data

--- powerpressi/src/enums/enums.ts
+++ powerpressi/src/enums/enums.ts
@@ -1,6 +1,6 @@
 export enum API{
-    apiUrl = 'http://localhost:86/wp-json/wp/v2/',
-    apiAuthUrl = 'http://localhost:86/wp-json/jwt-auth/v1/token',
-    apiUsername = 'mahlamusa',
-    apiPassword = '1WrxYVw(%VxGX6Op@z'
+    apiUrl = 'http://yourdomain.com/wp-json/wp/v2/',
+    apiAuthUrl = 'http://yourdomain.com/wp-json/jwt-auth/v1/token',
+    apiUsername = 'adminusername',
+    apiPassword = 'passwordforadminusername'
 }
